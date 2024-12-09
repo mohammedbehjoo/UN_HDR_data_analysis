29147dac1a97cc052a74b3d3f74571f81a13eeca
Mohammed <mohammedbehjooarchich@gmail.com>
2024-12-09 11:09:08 +0330

    feat: read the file. load the excel file as a dataframe. remove the unwanted columns.

--- eda.py
+++ eda.py
@@ -16,3 +16,22 @@ def cast_number(item):
         except (TypeError, ValueError):
             return item
     
+# check if the config file exits
+try:
+    print("the dataset exists: ",os.path.exists("config.env"),"\n")
+except:
+    print("the dataset file is either missing or empty\n")
+# load the config file from the config.env file
+load_dotenv("config.env")
+
+print(f"path to the dataset:\n{os.getenv('data_path')}","\n","-"*30)
+
+# excel filepath
+filepath=os.path.join(os.getenv("data_path"),"HDR23-24_Statistical_Annex_Tables_1-7.xlsx")
+print(f"file path is:\n{filepath}\n")
+
+# let's load the dataset file
+df_HDI=pd.read_excel(filepath,sheet_name="HDI")
+
+# let's filter and remove the columns that their names contain "unnamed".
+df_HDI=df_HDI.loc[:,~df_HDI.columns.str.contains("^Unnamed")]
