ceb85187665235dd01f025534cb47ebc1091c99c
Mohammed <mohammedbehjooarchich@gmail.com>
2024-12-09 12:01:04 +0330

    feat: create the output directories. create the output .txt file. write the output of the describe() into the output file for the df_HDI dataframe.

--- eda.py
+++ eda.py
@@ -3,6 +3,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+# a function for deleting every .txt file in the root directory and its sub-directories 
+def delete_txt_file(directory):
+    path=Path(directory)
+    for txt_file in path.rglob("*.txt"):
+        txt_file.unlink()
+        print(f"Deleted: {txt_file}\n","-"*30)
+
 
 # a function to cast to float and integer datatypes. use for desired columns.
 def cast_number(item):
@@ -24,14 +33,37 @@ except:
 # load the config file from the config.env file
 load_dotenv("config.env")
 
-print(f"path to the dataset:\n{os.getenv('data_path')}","\n","-"*30)
+# root directory of the output
+root_dir=os.getenv("root_dir_output")
+
+# directory for saving the result text file of the HDI dataframe
+save_txt_hdi=os.path.join(root_dir,"txt/hdi")
+os.makedirs(save_txt_hdi,exist_ok=True)
+print(f"the result txt director of HDI:\n{save_txt_hdi}\n","-"*30)
+
+# create the file of the txt output of HDI dataframe.
+df_hdi_txt=os.path.join(save_txt_hdi,"output.txt")
+
+# delete the .txt files before starting. it makes sure that every .txt file is being created from scratch
+delete_txt_file(root_dir)
+
+# check the path of the dataset
+print(f"path of the dataset:\n{os.getenv('data_path')}","\n","-"*30)
 
 # excel filepath
 filepath=os.path.join(os.getenv("data_path"),"HDR23-24_Statistical_Annex_Tables_1-7.xlsx")
-print(f"file path is:\n{filepath}\n")
+print(f"excel file path is:\n{filepath}\n")
 
 # let's load the dataset file
 df_HDI=pd.read_excel(filepath,sheet_name="HDI")
 
 # let's filter and remove the columns that their names contain "unnamed".
 df_HDI=df_HDI.loc[:,~df_HDI.columns.str.contains("^Unnamed")]
+
+# describe the dataset to have an insight of it.
+print(f"Describe df_HDI:\n{df_HDI.describe()}\n","-"*30)
+with open(df_hdi_txt,"a") as file:
+    file.write("Describe the df_HDI dataframe:\n")
+    file.write(df_HDI.describe().to_string())
+    file.write("\n"+"-"*30)
+    print(f"df_HDI describe is written to the file {df_hdi_txt}\n","-"*30)
