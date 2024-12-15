544996957a742d019e16d89b5412c68431d34dea
Mohammed <mohammedbehjooarchich@gmail.com>
2024-12-15 19:08:54 +0330

    feat: create a histogram chart for numeric column. different columns can be chosen for x and y axes.

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 import os
 import warnings
 
@@ -19,9 +20,40 @@ fl=st.file_uploader(":file_folder: Upload a file",type=(["xlsx"]))
 
 if fl is not None:
     filename=fl.name
-    # write the file's name to the app to make sure it is uploaded.
-    st.write(filename)
     # create the dataframe
-    df=pd.read_excel(filename)
-    st.write("Data Frame is read.")
+    df_hdi=pd.read_excel(filename)
+    st.write("Data Frame is created.")
 
+# create 2 columns for the app
+col1,col2=st.columns((2))
+
+
+# let's filter and remove the columns that their names contain "unnamed".
+df_hdi = df_hdi.loc[:, ~df_hdi.columns.str.contains("^Unnamed")]
+
+# let's rename the columns to have better namings
+df_hdi.rename(columns={"Human Development Index (HDI) ": "HDI",
+              "HDI rank.1": "HDI_rank"}, inplace=True)
+
+# cast the specified columns to the numeric values
+df_hdi[["HDI", "Expected years of schooling", "Mean years of schooling", "Gross national income (GNI) per capita", "GNI per capita rank minus HDI rank", "HDI_rank"]] = df_hdi[[
+    "HDI", "Expected years of schooling", "Mean years of schooling", "Gross national income (GNI) per capita", "GNI per capita rank minus HDI rank", "HDI_rank"]].apply(pd.to_numeric, errors="coerce")
+
+# drop the HDI rank column. it was redundant.
+df_hdi.drop("HDI rank", inplace=True, axis=1)
+
+# get rid rows with missing values
+df_hdi_clean = df_hdi.dropna()
+
+# get the numeric columns only
+numeric_columns=df_hdi_clean.select_dtypes(include="float64").columns
+
+# select a column for y value of the following chart
+
+# let's show the histogram relationship between columns
+with col1:
+    selected_column_x=st.selectbox("Choose a column for x axis:",options=numeric_columns,key="x")
+    selected_column_y=st.selectbox("Choose a column for y axis:",options=numeric_columns,key="y")
+    st.subheader(f"{selected_column_x} vs. {selected_column_y}")
+    fig=px.histogram(df_hdi_clean,x=selected_column_x,y=selected_column_y,template="seaborn",histfunc="avg")
+    st.plotly_chart(fig,use_container_width=True)
