d283335f4a2c6eb13fb67afa7e4d00db5b610929
Mohammed <mohammedbehjooarchich@gmail.com>
2024-12-30 12:11:54 +0330

    feat: add the 'how to navigate the app' section to the intro page.

--- dashboard_intro.py
+++ dashboard_intro.py
@@ -12,12 +12,14 @@ st.subheader("Exploring Global Development Indicators and Trends")
 
 st.subheader(":material/check_small: Did You Know?")
 # facts
-col1,col2,col3=st.columns(3)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.write("🇳🇴 Norway has consistently ranked as the country with the highest HDI in recent years.")
+    st.write(
+        "🇳🇴 Norway has consistently ranked as the country with the highest HDI in recent years.")
 with col2:
-    st.write("6️⃣0️⃣ More than 60% of countries have seen an improvement in HDI since 1990.")
+    st.write(
+        "6️⃣0️⃣ More than 60% of countries have seen an improvement in HDI since 1990.")
 with col3:
     st.write("👵🏿 Life expectancy at birth ranges from over 80 years in developed countries to under 60 in developing regions.")
 
@@ -61,17 +63,26 @@ st.markdown("- Interactive Visualizations\n -Comparisons across countries\n - Tr
 # Briefly describe the different pages (e.g., Data Exploration, Visual Analysis, Country Comparisons).
 # Mention any specific tools or filters, like dropdown menus.
 
-st.markdown("## ::material/double_aroow: How to navigate the app")
-
-
-
+st.markdown("## :material/double_arrow: How to navigate the app")
+st.markdown("""
+            ### Navigate the app 🚀
+            - 📌 **Introduction Page (You are here):**
+                - Learn about the HDR, its purpose, and the app's structure.
+            - 📊 **Exploratory Data Analysis (EDA) Page:**
+                - Visualize global trends and distributions.
+                - Interact with dropdown filters to view data for specific countries or regions.
+            - 📈 **Statistical Analysis Page:**
+                - Explore statistical relationships between variables (e.g., HDI vs. GNI per capita).
+                - Analyze correlations and regression models.
+            """)
+st.info("Start exploring by selecting a page from the sidebar! 📂")
 
 # dataset
 st.header(":material/double_arrow: Where to download the dataset?")
 st.markdown("You can download the dataset from [here](https://hdr.undp.org/content/human-development-report-2023-24). This is the United Nations\
     Development Programme (UNDP) website.")
 # create 3 columns
-_,col1,_=st.columns(3)
+_, col1, _ = st.columns(3)
 with col1:
     st.image("UNDP-Logo-Blue-Large-Transparent.png", caption="UNDP", width=75)
 
@@ -83,5 +94,5 @@ This app was built using:""")
 st.markdown("- **[Streamlit](https://streamlit.io/):** For building the interactive web application.\n![Streamlit](https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png)")
 st.markdown("- **[Plotly](https://plotly.com/):** For creating dynamic and interactive visualizations.\n![Plotly](https://images.plot.ly/logo/new-branding/plotly-logomark.png) ")
 st.markdown("- **[Matplotlib](https://matplotlib.org/):** For static data visualization.\n![Matplotlib](https://matplotlib.org/_static/images/logo2.svg)")
-st.markdown("- **[Dash](https://dash.plotly.com/):** For exploring layout designs and enhancing interactivity.")
-
+st.markdown(
+    "- **[Dash](https://dash.plotly.com/):** For exploring layout designs and enhancing interactivity.")
