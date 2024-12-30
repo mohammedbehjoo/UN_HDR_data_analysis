1a5c7c603072a864f9fcc81c5ea7c921f9cd14ce
Mohammed <mohammedbehjooarchich@gmail.com>
2024-12-30 18:30:43 +0330

    reafctor: create a function named plot_histogram(). clean the code.

--- pages/01_EDA.py
+++ pages/01_EDA.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -23,7 +24,7 @@ def load_data(file_name: str) -> pd.DataFrame:
     return pd.read_excel(file_name)
 
 
-def pre_process_data(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_data(df: pd.DataFrame) -> Tuple[pd.DataFrame,pd.Index]:
     # filter and remove the columns that their names contain "unnamed".
     df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
     # rename columns for clarity
@@ -31,14 +32,35 @@ def pre_process_data(df: pd.DataFrame) -> pd.DataFrame:
                    "HDI rank.1": "HDI_rank"}
     # rename the columns to have better namings
     df.rename(columns=rename_dict, inplace=True)
+    # Convert specified columns to numeric values
+    numeric_columns_to_convert = [
+        "HDI", 
+        "Expected years of schooling", 
+        "Mean years of schooling", 
+        "Gross national income (GNI) per capita", 
+        "GNI per capita rank minus HDI rank", 
+        "HDI_rank"
+    ]
     # cast the specified columns to the numeric values
-    df [["HDI", "Expected years of schooling", "Mean years of schooling", "Gross national income (GNI) per capita", "GNI per capita rank minus HDI rank", "HDI_rank"]] = df[[
-        "HDI", "Expected years of schooling", "Mean years of schooling", "Gross national income (GNI) per capita", "GNI per capita rank minus HDI rank", "HDI_rank"]].apply(pd.to_numeric, errors="coerce")
+    df [numeric_columns_to_convert] = df[numeric_columns_to_convert].apply(pd.to_numeric, errors="coerce")
     # drop the HDI rank column. it was redundant.
-    df.drop("HDI rank", inplace=True, axis=1)
+    if "HDI rank" in df.columns:
+        df.drop("HDI rank", inplace=True, axis=1)
     # get rid rows with missing values
     df = df.dropna()
-    return df
+    # get the numeric columns only
+    numeric_columns = df.select_dtypes(include="float64").columns
+    # exclude the last column
+    numeric_columns=numeric_columns[:-1]
+    return df,numeric_columns
+
+
+def plot_histogram(df:pd.DataFrame,selected_column_x,selected_column_y,histfunc="avg") ->None:
+    fig = px.histogram(df, x=selected_column_x,
+                       y=selected_column_y, template="seaborn", histfunc="avg")
+    st.plotly_chart(fig)
+
+
 
 
 if __name__ == "__main__":
@@ -64,4 +86,17 @@ if __name__ == "__main__":
     # multiply the poplulation column by one million.
     pop_gnipc_df["Population"] = pop_gnipc_df["Population"] * 1000000
     
+    clean_data,numeric_columns=preprocess_data(raw_data)
     
+    # create 2 columns
+    col1,col2=st.columns(2)
+    
+    with col1:
+        selected_column_x=st.selectbox(
+            "Choose a column for x axis:",options=numeric_columns,key="x"
+        )
+        selected_column_y=st.selectbox(
+            "Choose a column for y axis:",options=numeric_columns,key="y"
+        )
+        st.subheader(f"{selected_column_x} vs. {selected_column_y}")
+        plot_histogram(clean_data,selected_column_x,selected_column_y)
